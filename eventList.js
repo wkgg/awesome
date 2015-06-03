ffffe0b139ad1339940d237fca35c9354f71c95a
Han Feng <hfcc8685@gmail.com>
2015-06-03 20:33:08 +0800

    [hf] add eventList

---
+++ eventList.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  Image,
+  ListView,
+  TouchableHighlight,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var EventList = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows([
+         {
+          imageUri: "https://download.unsplash.com/photo-1430760814266-9c81759e5e55",
+          title: "WWDC 2015",
+          address: "apple",
+          time: "2015-01-01"
+        },
+        {
+          imageUri: "https://download.unsplash.com/photo-1429032021766-c6a53949594f",
+          title: "hello world!",
+          address: "world map",
+          time: "2015-01-01"
+        },
+        {
+          imageUri: "https://download.unsplash.com/photo-1431184052543-809fa8cc9bd6",
+          title: "hello world!",
+          address: "world map",
+          time: "2015-01-01"
+        }
+      ]),
+    };
+  },
+  _renderRow: function(rowData) {
+    return (         
+        <View style={styles.row}>
+            <Image style={styles.image} source={{uri: rowData.imageUri}} />
+            <Text style={styles.title}>{rowData.title}</Text>
+            <Text style={styles.address}>{rowData.address}</Text>
+            <Text style={styles.time}>{rowData.time}</Text>
+        </View>
+    );
+  },
+  render: function() {
+    return(
+      <ListView dataSource={this.state.dataSource} renderRow={this._renderRow}/>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+    row: {
+      padding: 10,
+      marginBottom: 10
+    },
+    image: {
+      height: 200,
+      marginBottom: 5
+    },
+    title: {
+      fontSize: 18,
+      marginBottom: 5
+    },
+    address: {
+      marginBottom: 5
+    }
+});
+
+module.exports = EventList;
