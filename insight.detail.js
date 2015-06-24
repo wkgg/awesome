226b8dc38ce41d1bce3179e3a5c7a7ea89c017c6
xuhaiming <haiming12322@gmail.com>
2015-06-24 16:42:51 +0800

    Show insight detail with webview

--- insight.detail.js
+++ insight.detail.js
@@ -7,6 +7,7 @@ var {
     View,
     Text,
     StyleSheet,
+    ScrollView,
     WebView
     } = React;
 
@@ -16,54 +17,59 @@ var FAKE_INSIGHT = {
 };
 
 var InsightDetail = React.createClass({
-    getInitialState: function(){
-        return({
-            insight: ""
-        }
-        );
+    getHtml: function (title, content) {
+        var cssStyle = '<style>h1{font-size: 20px;}img{width:100%;height:200px;}</style>';
+        return '<!DOCTYPE html><html><body>' + cssStyle + '<h1>' + title + '</h1>' + content + '</body></html>';
     },
-    componentDidMount: function(){
-        insightRepository.getById(this.props.id, function(data){
+    getInitialState: function () {
+        return ({
+            insight: ""
+        });
+    },
+    componentDidMount: function () {
+        insightRepository.getById(this.props.id, function (data) {
             this.setState({insight: data})
         }.bind(this));
     },
     render: function () {
-        if(this.state.insight != ""){
+        if (this.state.insight != "") {
             var insight = this.state.insight;
             return (
-              <View>
-                <Text style={styles.title}>{insight.get("title")}</Text>
-                <WebView
-                    style={styles.webView}
-                    ref={'webview'}
-                    html={insight.get("content")}
-                    automaticallyAdjustContentInsets={false} />
-              </View>
+                <View>
+                    <ScrollView>
+                        <Text style={styles.title}>{insight.get("title")}</Text>
+                        <WebView
+                            style={styles.webView}
+                            ref={'webview'}
+                            html={this.getHtml('', insight.get("content"))}
+                            scrollEnabled={true}
+                            automaticallyAdjustContentInsets={false}/>
+                    </ScrollView>
+                </View>
             )
         }
-        /* need improve later*/
-        return null;       
+        return null;
     }
 });
 
 var styles = StyleSheet.create({
     insightDetail: {
-        alignItems: 'center',
-        justifyContent: 'center'
+        flex: 1,
+        flexDirection: 'column'
     },
     title: {
         fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
+        marginTop: 10,
+        textAlign: 'center'
     },
     content: {
         marginTop: 10
     },
     webView: {
         marginTop: 10,
-        height: 500,
         backgroundColor: 'white',
-    },
+        height: 500
+    }
 });
 
 module.exports = InsightDetail;
